9063a6dd96f58d7bed278687e415ce3df0e5f58e
Kuhandran <skuhandran@yahoo.com>
2024-02-04 22:35:03 +0800

    Updated as the background

---
+++ app/routes/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import WaterSplash from './../components/waterSplash';
+// import { Canvas } from 'react-three-fiber';
+
+export default function header() {
+    return (
+          <WaterSplash />
+    );
+}
