2e5fba55d864495bcd7957d1ae6740c35b958d52
Kuhandran <skuhandran@yahoo.com>
2024-02-05 18:10:11 +0800

    Fixed Header Changes

--- app/components/HeaderBackground/HeaderBackground.jsx
+++ app/components/HeaderBackground/HeaderBackground.jsx
@@ -1,14 +1,14 @@
 // Header.js
 import React from 'react';
 import './HeaderBackground.css';
-import WaterSplash from "../WaterSplash/WaterSplash";
+import {WaterSplash} from "../WaterSplash/WaterSplash";
 import HeaderPage from "../HeaderPage/HeaderPage";
 
 
 const HeaderBackground = () => {
   return (
     <div>
-        { <WaterSplash/>}
+         <WaterSplash/>
         <div>
             <HeaderPage/>
         </div>
