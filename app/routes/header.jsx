a581e4ddd09f6d2fce3a35da97b1e3e2a191bb6e
Kuhandran <skuhandran@yahoo.com>
2024-02-12 11:04:18 +0800

    Updated with footer table and contents

--- app/routes/header.jsx
+++ app/routes/header.jsx
@@ -4,6 +4,7 @@ const HeaderBackground = lazy(() => import("../components/HeaderBackground/Heade
 const HeaderNavigation = lazy(() => import("../components/HeaderNavigation/HeaderNavigation"));
 const ContentContainer = lazy(() => import("../components/ContentContainer/ContentContainer"));
 const MilestoneContainer = lazy(() => import("../components/MilestoneContainer/MilestoneContainer"));
+const FooterContainer = lazy(() => import("../components/FooterContainer/FooterContainer"));
 // import { Canvas } from 'react-three-fiber';
 
 export default function Headers() {
@@ -13,6 +14,7 @@ export default function Headers() {
             <HeaderBackground />
             <ContentContainer/>
             <MilestoneContainer/>
+            <FooterContainer/>
         </React.Fragment>
 
     );
