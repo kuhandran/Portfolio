cd6d8f83e8d53f964bb3cfc6297759ae4b7ef690
Kuhandran <skuhandran@yahoo.com>
2024-09-15 18:04:46 +0800

    updated the public folder

---
+++ public/build/_shared/FooterContainer-ESXWLQFQ.js
@@ -0,0 +1,107 @@
+import {
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-XU7DNSPJ.js";
+import "/build/_shared/chunk-BOXFZXVX.js";
+import {
+  createHotContext
+} from "/build/_shared/chunk-TSBYFMHC.js";
+import "/build/_shared/chunk-UWV35TSL.js";
+import {
+  __toESM
+} from "/build/_shared/chunk-PNG5AS42.js";
+
+// app/components/FooterContainer/FooterContainer.jsx
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime(), 1);
+if (!window.$RefreshReg$ || !window.$RefreshSig$ || !window.$RefreshRuntime$) {
+  console.warn("remix:hmr: React Fast Refresh only works when the Remix compiler is running in development mode.");
+} else {
+  prevRefreshReg = window.$RefreshReg$;
+  prevRefreshSig = window.$RefreshSig$;
+  window.$RefreshReg$ = (type, id) => {
+    window.$RefreshRuntime$.register(type, '"app/components/FooterContainer/FooterContainer.jsx"' + id);
+  };
+  window.$RefreshSig$ = window.$RefreshRuntime$.createSignatureFunctionForTransform;
+}
+var prevRefreshReg;
+var prevRefreshSig;
+if (import.meta) {
+  import.meta.hot = createHotContext(
+    //@ts-expect-error
+    "app/components/FooterContainer/FooterContainer.jsx"
+  );
+  import.meta.hot.lastModified = "1707706871274.6929";
+}
+var FooterContainer = () => {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "flex flex-col min-h-screen fixed bottom-0 left-0 sticky top-[100vh]", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("footer", { className: "flex flex-col items-center bg-neutral-900 text-center text-white sticky top-[100vh]", children: [
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "container px-6 pt-6", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "mb-6 flex justify-center", children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { href: "#!", type: "button", className: "m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0", "data-te-ripple-init": true, "data-te-ripple-color": "light" }, void 0, false, {
+        fileName: "app/components/FooterContainer/FooterContainer.jsx",
+        lineNumber: 28,
+        columnNumber: 25
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { href: "#!", type: "button", className: "m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0", "data-te-ripple-init": true, "data-te-ripple-color": "light" }, void 0, false, {
+        fileName: "app/components/FooterContainer/FooterContainer.jsx",
+        lineNumber: 32,
+        columnNumber: 25
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { href: "#!", type: "button", className: "m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0", "data-te-ripple-init": true, "data-te-ripple-color": "light" }, void 0, false, {
+        fileName: "app/components/FooterContainer/FooterContainer.jsx",
+        lineNumber: 36,
+        columnNumber: 25
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { href: "#!", type: "button", className: "m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0", "data-te-ripple-init": true, "data-te-ripple-color": "light" }, void 0, false, {
+        fileName: "app/components/FooterContainer/FooterContainer.jsx",
+        lineNumber: 40,
+        columnNumber: 25
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { href: "#!", type: "button", className: "m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0", "data-te-ripple-init": true, "data-te-ripple-color": "light" }, void 0, false, {
+        fileName: "app/components/FooterContainer/FooterContainer.jsx",
+        lineNumber: 44,
+        columnNumber: 25
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { href: "#!", type: "button", className: "m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0", "data-te-ripple-init": true, "data-te-ripple-color": "light" }, void 0, false, {
+        fileName: "app/components/FooterContainer/FooterContainer.jsx",
+        lineNumber: 48,
+        columnNumber: 25
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/components/FooterContainer/FooterContainer.jsx",
+      lineNumber: 27,
+      columnNumber: 21
+    }, this) }, void 0, false, {
+      fileName: "app/components/FooterContainer/FooterContainer.jsx",
+      lineNumber: 26,
+      columnNumber: 17
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "w-full p-4 text-center", children: [
+      "\xA9 2023 Copyright:",
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { className: "text-whitehite", href: "https://tw-elements.com/", children: "KS Porfolio Powered by Tailwind & Remix" }, void 0, false, {
+        fileName: "app/components/FooterContainer/FooterContainer.jsx",
+        lineNumber: 53,
+        columnNumber: 74
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/components/FooterContainer/FooterContainer.jsx",
+      lineNumber: 53,
+      columnNumber: 17
+    }, this)
+  ] }, void 0, true, {
+    fileName: "app/components/FooterContainer/FooterContainer.jsx",
+    lineNumber: 25,
+    columnNumber: 13
+  }, this) }, void 0, false, {
+    fileName: "app/components/FooterContainer/FooterContainer.jsx",
+    lineNumber: 24,
+    columnNumber: 10
+  }, this);
+};
+_c = FooterContainer;
+var FooterContainer_default = FooterContainer;
+var _c;
+$RefreshReg$(_c, "FooterContainer");
+window.$RefreshReg$ = prevRefreshReg;
+window.$RefreshSig$ = prevRefreshSig;
+export {
+  FooterContainer_default as default
+};
+//# sourceMappingURL=/build/_shared/FooterContainer-ESXWLQFQ.js.map
---
+++ public/build/root-V7VEZ7N7.js
@@ -0,0 +1,114 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "/build/_shared/chunk-M3SCSKV2.js";
+import "/build/_shared/chunk-GIAAE3CH.js";
+import {
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-XU7DNSPJ.js";
+import "/build/_shared/chunk-BOXFZXVX.js";
+import {
+  createHotContext
+} from "/build/_shared/chunk-TSBYFMHC.js";
+import "/build/_shared/chunk-UWV35TSL.js";
+import {
+  __toESM
+} from "/build/_shared/chunk-PNG5AS42.js";
+
+// css-bundle-plugin-ns:@remix-run/css-bundle
+var cssBundleHref = "/build/css-bundle-O6EQ64VU.css";
+
+// app/root.tsx
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime(), 1);
+if (!window.$RefreshReg$ || !window.$RefreshSig$ || !window.$RefreshRuntime$) {
+  console.warn("remix:hmr: React Fast Refresh only works when the Remix compiler is running in development mode.");
+} else {
+  prevRefreshReg = window.$RefreshReg$;
+  prevRefreshSig = window.$RefreshSig$;
+  window.$RefreshReg$ = (type, id) => {
+    window.$RefreshRuntime$.register(type, '"app/root.tsx"' + id);
+  };
+  window.$RefreshSig$ = window.$RefreshRuntime$.createSignatureFunctionForTransform;
+}
+var prevRefreshReg;
+var prevRefreshSig;
+if (import.meta) {
+  import.meta.hot = createHotContext(
+    //@ts-expect-error
+    "app/root.tsx"
+  );
+}
+var meta = () => [{
+  charset: "utf-8",
+  title: "Kuhandran Portfolio",
+  viewport: "width=device-width,initial-scale=1"
+}];
+var links = () => [...cssBundleHref ? [{
+  rel: "stylesheet",
+  href: cssBundleHref
+}] : []];
+function App() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("html", { lang: "en", children: [
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("head", { children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Meta, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 36,
+        columnNumber: 9
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Links, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 37,
+        columnNumber: 9
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/root.tsx",
+      lineNumber: 35,
+      columnNumber: 7
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("body", { children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Outlet, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 40,
+        columnNumber: 9
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(ScrollRestoration, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 41,
+        columnNumber: 9
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Scripts, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 42,
+        columnNumber: 9
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(LiveReload, {}, void 0, false, {
+        fileName: "app/root.tsx",
+        lineNumber: 43,
+        columnNumber: 9
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/root.tsx",
+      lineNumber: 39,
+      columnNumber: 7
+    }, this)
+  ] }, void 0, true, {
+    fileName: "app/root.tsx",
+    lineNumber: 34,
+    columnNumber: 10
+  }, this);
+}
+_c = App;
+var _c;
+$RefreshReg$(_c, "App");
+window.$RefreshReg$ = prevRefreshReg;
+window.$RefreshSig$ = prevRefreshSig;
+export {
+  App as default,
+  links,
+  meta
+};
+//# sourceMappingURL=/build/root-V7VEZ7N7.js.map
